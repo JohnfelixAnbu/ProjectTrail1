7905a99bf42507a33ce5f72fc28568201a54880d
JohnfelixAnbu <johnfelixi586@gmail.com>
2021-04-25 13:28:22 +0530

    Adding demo project into master branch

---
+++ src/org/in/ThisYear.java
@@ -0,0 +1,34 @@
+package org.in;
+public class ThisYear {
+private void johnRichard() {
+System.out.println("73");
+}
+private void irshelenaBai() {
+System.out.println("63");
+}
+private void johnFelix() {
+System.out.println("29");
+}
+private void johnAlex() {
+System.out.println("28");
+}
+public static void main(String[] args) {
+	byte age=24;
+	System.out.println("patien age is"+age);
+	ThisYear AGE=new ThisYear();
+	AGE.johnRichard();
+	AGE.irshelenaBai();
+	AGE.johnFelix();
+	AGE.johnAlex();
+}
+
+}
+
+
+
+
+	
+
+
+
+
